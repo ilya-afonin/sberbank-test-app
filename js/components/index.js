ac3cd9f7d0b19fab0cb4f8c66e8118a7004d285c
Ilya Afonin <rjiabkom@gmail.com>
2019-09-07 00:48:03 +0300

    Update 2019-09-06T21:47:59.643Z

--- js/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as SearchBar } from './SearchBar';
-export { default as DataBar } from './DataBar';
-export { default as UserTable } from './UserTable';
-export { default as UserDetail } from './UserDetail';
--- js/constants/InitialState.js
+++
@@ -1,6 +0,0 @@
-export const INITIAL_STATE = {
-  data: [],
-  filteredData: [],
-  isFetching: true,
-  didInvalidate: false
-};
